bdcecf8f991e9bd51b1059a1173e9a6ca8788067
DK <87054926+kdkcode@users.noreply.github.com>
2021-08-03 18:45:11 +0000

    다이나믹 문제-개민전사 commit

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+#개미전사
+
+#답안
+
+n=int(input('식량창고 갯수 입력: '))
+array = list(map(int,input().split()))
+
+
+d=[0]*100
+
+d[0]=array[0]
+d[1]=max(array[0],array[1])
+for i in range(2,n):
+  d[i] = max(d[i-1],d[i-2]+array[i])
+
+
+print(d[n-1])
+
+
+
 
 
 
